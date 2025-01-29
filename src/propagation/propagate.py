09eba3ef5e8fb5e771e16c63bd6111878b31a56b
Justin Murante <jmurante@arizona.edu>
2025-01-28 20:48:48 -0700

    project framework

---
+++ src/propagation/propagate.py
@@ -0,0 +1,86 @@
+from numpy.fft import fft2, ifft2
+from numpy import exp, arange, meshgrid
+from src.propagation.transfer_functions import free_space_transfer_function
+
+
+def mplc_propagate(wavefront, mask, **kwargs):
+    """
+    Takes an wavefront and phase mask and finds the resulting field after
+    propagating some distance
+
+    :param wavefront:
+        The wavefront incident on the phase mask
+
+    :param mask:
+        The phase mask (values between 0 and 2pi)
+
+    :param **kwargs
+        parameters for the free space propagation
+    """
+
+    # Compute the modulated wavefront
+    modulated_wavefront = wavefront * exp(1j * mask)
+
+    # Compute the free space propagation
+    return free_space_propagate(modulated_wavefront, **kwargs)
+
+
+def free_space_propagate(wavefront, h=None,
+                         wavelength=None, distance=None,
+                         x=None, y=None, dx=None, dy=None):
+    """
+    Takes an wavefront and phase mask and finds the resulting field after
+    propagating some distance
+
+    :param wavefront:
+        The wavefront incident on the phase mask
+
+    :param h: NDArray
+        Free space transfer function.
+        If this is inputted, none of the other kwargs are necessary
+
+    :param wavelength:
+        Wavelength of the light
+
+    :param distance:
+        Distance of propagation
+
+    :param x: NDArray
+        x coordinate meshgrid
+
+    :param y: NDArray
+        y coordinate meshgrid
+
+    :param dx: float
+        Sampling period in the x direction
+
+    :param dy: float
+        Sampling period in the y direction
+
+    :return:
+        Resulting wavefront
+    """
+
+    # Take the Fourier transform of the wavefront
+    wavefront_ft = fft2(wavefront)
+
+    # Check for pre-inputted h
+    if h is None:
+        # Compute x and y if necessary
+        if x is None or y is None:
+            if dx is None or dy is None:
+                raise SyntaxError('Must either input both x and y '
+                                  'or both dx and dy')
+            Ny, Nx = wavefront.shape
+            x = dx * (arange(Nx) - (Nx / 2))
+            y = dy * (arange(Ny) - (Ny / 2))
+            x, y = meshgrid(x, y)
+
+        # Obtain the free space transfer function
+        h = free_space_transfer_function(x, y, distance, wavelength)
+
+    # Multiply by the free space transfer function
+    wavefront_ft *= h
+
+    # Inverse Fourier Transform
+    return ifft2(wavefront_ft)
---
+++ src/propagation/transfer_functions.py
@@ -0,0 +1,76 @@
+from nptyping import NDArray
+from numpy import min, max, meshgrid, pi, sqrt, exp, arange
+from numpy.fft import fftfreq, fftshift, ifftshift
+
+
+def compute_FSTF(input_field, output_field, dx, dy,
+                 distance, wavelength, k_space_filter: float = 1.0):
+
+    # Ensure the input and output fields have the same shape
+    shape = input_field.shape
+    if shape != output_field.shape:
+        raise SyntaxError('Input and Output fields must have the same shape')
+
+    # Calculate the x and y coordinates
+    Ny, Nx = shape
+    x = dx * (arange(Nx) - (Nx / 2))
+    y = dy * (arange(Ny) - (Ny / 2))
+    x, y = meshgrid(x, y)
+
+    # Compute the free space transfer function for the given distance
+    h = free_space_transfer_function(x, y, distance, wavelength)
+
+    # Filter the transfer function
+    if k_space_filter < 1:
+        radius = sqrt(x ** 2 + y ** 2)
+        max_radius = max(radius)
+        frequency_mask = radius < max_radius * k_space_filter
+        h *= ifftshift(frequency_mask)
+
+    # return the result
+    return h
+
+
+def free_space_transfer_function(x: NDArray, y: NDArray, z,
+                                 wavelength, shift=False):
+    """
+    This is the free space transfer function used by Joel Carpenter
+
+    :param x: NDArray
+        X coordinate meshgrid
+
+    :param y: NDArray
+        Y coordinate meshgrid
+
+    :param z: float
+        Propagation distance in the z direction
+
+    :param wavelength: float
+        Wavelength of light
+
+    :param shift: boolean
+        Select whether or not to shift the k coordinates
+
+    :return: NDArray
+        Transfer function of free space
+    """
+
+    # Get x and y coordinates
+    ny, nx = x.shape
+    dx = (max(x) - min(x)) / (nx - 1)
+    dy = (max(y) - min(y)) / (ny - 1)
+
+    # Get k-space coordinates for the Fourier transform
+    if shift:
+        # kx = (arange(Nx) - (Nx / 2)) / (max(x) - min(x))
+        kx = fftshift(fftfreq(nx, d=dx))
+        ky = fftshift(fftfreq(ny, d=dy))
+    else:
+        kx = fftfreq(nx, d=dx)
+        ky = fftfreq(ny, d=dy)
+    kx, ky = meshgrid(kx, ky)
+
+    # Compute the transfer function
+    exponent = -1j * 2 * pi * sqrt(
+        (wavelength ** (-2)) + (kx ** 2) + (ky ** 2))
+    return exp(exponent * z)
